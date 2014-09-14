5b3b551424088295fc74008cff4faba9592d5527
JarekDuda <dudaj@interia.pl>
2014-09-14 08:27:43 +0200

    added spread_uABS()
    uABS has direct encoding/decoding formulas for binary alphabet

--- ANStoolkit.cpp
+++ ANStoolkit.cpp
@@ -176,6 +176,13 @@ struct ANS {
    sort(v.begin(),v.end());
    for(int i=0;i<L;i++) s[i]=v.at(i).second;
 }
+ void spread_uABS(){     // only for binary alphabet!  coding/decoding given by arithmetic formula
+      delete[] s; s = new avar[L];
+     if(m != 2) cout << "Error: only binary alphabet here";
+     else { for(int i=0; i<L; i++) s[i] = ceil((float)(i+L+1)*p[1]) - ceil((float)(i+L) * p[1]);}
+     int sum=0;  for(int i=0; i<L; i++) sum+=s[i]; q[1] = sum; q[0] = L-sum;    // find used q[]
+ }
+ 
  // ---------- SCRAMBLERS - disturb symbol spread accordingly to cryptokey ----
  void scrambler0(uint key){            //randomly switches symbols on 2i-1 and 2i positions
      srand(key);rand();                // can be replaced with a better PRNG
@@ -274,12 +281,12 @@ ANS init_rand_unif(int n){   // simple random distribution
 
 int main()    // currently: single test
 {// choose probability distribution
-ANS test=init_binary(0.2,2);         // n binary  variables,  m=2^n             
+ANS test=init_binary(0.2,1);         // n binary  variables,  m=2^n             
 // ANS test=init_power(0.99,256);    // p_i ~ rho^i,  m=256
 //ANS test=init_rand_unif(256);      // m = 256
 // test.printp();
 // sort(test.p,test.p+test.m,greater<float>());
-test.quantize_prec(4);                        // choose quantization
+test.quantize_prec(5);                        // choose quantization
 // test.quantize_fast(12);                      // L = 2^value
 // test.printq();
 int sum=0; for(int i=0;i<test.m;i++)sum+=test.q[i];   // test quantizer
@@ -302,14 +309,14 @@ if(test.m < 20) test.printq();
 cout<<"Entropy for QUANTIZATION grows to " <<test.hq<< " ~ (1 + "<<(test.hq-test.h)/test.h<<")H bits/symbol"<< endl;
 cout<<"Then perform symbol spread"<<endl;
 
+if(test.m==2){cout<<"spread_uABS() - "; test.spread_uABS(); test.find_sp();
+    test.prints(); cout<<"Entropy for its tANS is " <<test.hANS<< " ~ (1 + "<<(test.hANS-test.h)/test.h<<")H bits/symbol"<<endl;}
 cout<<"spread_range_i() - "; test.spread_range_i(); test.find_sp();
 if(test.m < 11) test.prints();
 cout<<"Entropy for its tANS is " <<test.hANS<< " ~ (1 + "<<(test.hANS-test.h)/test.h<<")H bits/symbol"<<endl;
-test.printsp();
 cout<<"spread_range_d() - "; test.spread_range_d(); test.find_sp();
 if(test.m < 11) test.prints();
 cout<<"Entropy for its tANS is " <<test.hANS<< " ~ (1 + "<<(test.hANS-test.h)/test.h<<")H bits/symbol"<<endl;
-test.printsp();
 cout<<"spread_fast() - "; test.spread_fast(); test.find_sp();
 if(test.m < 11) test.prints();
 cout<<"Entropy for its tANS is " <<test.hANS<< " ~ (1 + "<<(test.hANS-test.h)/test.h<<")H bits/symbol"<<endl;
